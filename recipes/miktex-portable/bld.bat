c7d2790816f0ff6ce0c7309608bc7102caf278c6
Jan Schulz <jasc@gmx.net>
2016-04-27 20:30:30 +0200

    Update scripts

--- recipes/miktex-portable/bld.bat
+++ recipes/miktex-portable/bld.bat
@@ -3,13 +3,12 @@
 7za x miktex-portable-%PKG_VERSION%.exe -o%LIBRARY_PREFIX%\miktex-portable
 if errorlevel 1 exit 1
 
-rem latex tools must be run from miktex tree
-rem nbconvert uses pdflatex & bibtex
-rem SCRIPTS dir is created by 7za install
 
+rem SCRIPTS dir should already be created by 7za install
 mkdir "%SCRIPTS%"
 if errorlevel 1 exit 1
 
+rem latex tools must be run from miktex tree
 for %%f in ("%LIBRARY_PREFIX%\miktex-portable\miktex\bin\*.exe") do (
 	echo @%%~dp0\..\Library\miktex-portable\miktex\bin\%%~nf %%* >> "%SCRIPTS%\%%~nf.bat"
 )
